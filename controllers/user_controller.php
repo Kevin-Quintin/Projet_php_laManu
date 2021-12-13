388a031f0166b591e6fe2f622553212b52b13305
Kevin-dev-afpa <kequintin@laposte.net>
2021-12-13 12:19:16 +0100

    Ajout de variable pour déterminer le serveur et le chemin des cookies

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -1,14 +1,16 @@
 <?php 
 if(isset($_POST['desinscrire'])){
 
-    unset($_COOKIE["Nom"]);
-    setcookie("Nom", " ", time() - 0, "/projet_php_laManu", "lamanu");
-    setcookie("Prenom", " ", time() - 0, "/projet_php_laManu", "lamanu");
-    setcookie("Age", " ", time() - 0, "/projet_php_laManu", "lamanu");
-    setcookie("Genre", " ", time() - 0, "/projet_php_laManu", "lamanu");
-    setcookie("Courriel", " ", time() - 0, "/projet_php_laManu", "lamanu");
-    setcookie("Code_postal", " ", time() - 0, "/projet_php_laManu", "lamanu");
-    setcookie("Type_recherche", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    $domain = $_SERVER['SERVER_NAME'];
+    $path = explode("/", $_SERVER["REQUEST_URI"]);
+
+    setcookie("Nom", " ", time() - 0, "/".$path[1], $domain);
+    setcookie("Prenom", " ", time() - 0, "/".$path[1], $domain);
+    setcookie("Age", " ", time() - 0, "/".$path[1], $domain);
+    setcookie("Genre", " ", time() - 0, "/".$path[1], $domain);
+    setcookie("Courriel", " ", time() - 0, "/".$path[1], $domain);
+    setcookie("Code_postal", " ", time() - 0, "/".$path[1], $domain);
+    setcookie("Type_recherche", " ", time() - 0, "/".$path[1], $domain);
 
     header('location:../index.php');
     }
