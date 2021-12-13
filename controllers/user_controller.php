cb30f82ab1cb993f0db17ef24352d28d0632cf62
Kevin-dev-afpa <kequintin@laposte.net>
2021-12-13 12:00:23 +0100

    Suppression des cookies

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -1,13 +1,14 @@
 <?php 
 if(isset($_POST['desinscrire'])){
 
-    setcookie("Nom", " ", time() - 3600, "/");
-    setcookie("Prenom", " ", time() - 3600, "/");
-    setcookie("Age", " ", time() - 3600, "/");
-    setcookie("Genre", " ", time() - 3600, "/");
-    setcookie("Courriel", " ", time() - 3600, "/");
-    setcookie("Code_postal", " ", time() - 3600, "/");
-    setcookie("Type_recherche", " ", time() - 3600, "/");
+    unset($_COOKIE["Nom"]);
+    setcookie("Nom", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    setcookie("Prenom", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    setcookie("Age", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    setcookie("Genre", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    setcookie("Courriel", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    setcookie("Code_postal", " ", time() - 0, "/projet_php_laManu", "lamanu");
+    setcookie("Type_recherche", " ", time() - 0, "/projet_php_laManu", "lamanu");
 
     header('location:../index.php');
     }
